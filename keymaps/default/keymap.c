d23118fcda1ff4918aa603da299d32510fd4df24
Ian Merrick <MerrickI@Cardiff.ac.uk>
2026-01-21 23:58:48 +0000

    There wa an unexpected #~ output on the row 4 backslash key. Now using ISO‑specific keycodes to fix this.

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*R1*/  KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,          KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,               KC_DEL,
 /*R2*/  KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_ENT,                KC_HOME,
 /*R3*/  KC_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NONUS_HASH,                 KC_END,
-/*R4*/  KC_LSFT,KC_BSLS,KC_Z,   KC_X,   KC_C,   KC_V,                 KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT, KC_UP,
+/*R4*/  KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,                 KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT, KC_UP,
 /*R5*/  KC_LCTL,KC_LGUI,KC_LALT,       SPCL,   KC_B,           SPCR,                          KC_RALT, MO(_FN),KC_RCTL, KC_LEFT,KC_DOWN,KC_RGHT
 ),
 
